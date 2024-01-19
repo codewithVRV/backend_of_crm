cfb99ef7abd58779f3ee28228148c25aae706ded
Vishnu Kumar <vermaji1891@gmail.com>
2024-01-19 17:00:20 +0530

    added cors

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const ticketRoutes = require('./routes/ticket.routes');
 cors.use({
     origin: "*"
 })
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-access-token");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
+//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-access-token");
+//     next();
+// });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 authRoutes(app);
